7eb6b08bc20ec6beed3e5b9c0b5648c0ff9f12e5
vincentappiah <appiahv@rocketmail.com>
2021-02-14 17:39:15 +0000

    Add files via upload

---
+++ test.py
@@ -0,0 +1,19 @@
+
+print("________________________________________")
+
+import matplotlib
+print('matplotlib version:%s'%matplotlib.__version__)
+print('matplotlib path:%s'%matplotlib.__file__)
+print("________________________________________")
+import Bio
+print('Biopython version:%s'%Bio.__version__)
+print('Biopython path:%s'%Bio.__file__)
+print("________________________________________")
+import pydot
+print('pydot version:%s'%pydot.__version__)
+print('pydot path:%s'%pydot.__file__)
+print("________________________________________")
+import networkx
+print('networkx version:%s'%networkx.__version__)
+print('networkx path:%s'%networkx.__file__)
+#print("________________________________________")
