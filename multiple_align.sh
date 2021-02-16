12b25b9b805fc2ad35a4570984be77d2e25191b7
vincentappiah <appiahv@rocketmail.com>
2021-02-16 04:03:19 +0000

    Add files via upload

--- multiple_align.sh
+++ multiple_align.sh
@@ -4,7 +4,7 @@ multi_fasta=$1
 
 bname=$(basename $multi_fasta .fasta)
 apps/muscle -in $multi_fasta -out ./${bname}.msa.fasta
-apps/muscle -in ./${bname}.msa.fasta -out ./${bname}.refined.phylip -phyi
+apps/muscle -in ./${bname}.msa.fasta -out ./${bname}.refined.phylip -refine -phyi
 
 echo "File saved as ${bname}.refined.phylip"
 
