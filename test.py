3d195bf8fec4f2184984cda8db59038c74f02020
vincentappiah <appiahv@rocketmail.com>
2021-02-17 16:58:46 +0000

    Update test.py

--- test.py
+++ test.py
@@ -3,17 +3,17 @@ print("________________________________________")
 
 import matplotlib
 print('matplotlib version:%s'%matplotlib.__version__)
-print('matplotlib path:%s'%matplotlib.__file__)
+print('matplotlib path:/home/user/desktop/create-phylogenetic-trees/phylo/lib/python3.6/site-packages/matplotlib/__init__.py)
 print("________________________________________")
 import Bio
 print('Biopython version:%s'%Bio.__version__)
-print('Biopython path:%s'%Bio.__file__)
+print('Biopython path:/home/user/desktop/create-phylogenetic-trees/phylo/lib/python3.6/site-packages/Bio/__init__.py')
 print("________________________________________")
 import pydot
 print('pydot version:%s'%pydot.__version__)
-print('pydot path:%s'%pydot.__file__)
+print('pydot path:/home/user/desktop/create-phylogenetic-trees/phylo/lib/python3.6/site-packages/pydot.py')
 print("________________________________________")
 import networkx
 print('networkx version:%s'%networkx.__version__)
-print('networkx path:%s'%networkx.__file__)
+print('networkx path:/home/user/desktop/create-phylogenetic-trees/phylo/lib/python3.6/site-packages/networkx/__init__.py')
 #print("________________________________________")
