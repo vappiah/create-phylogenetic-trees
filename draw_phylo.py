06346cc602cb255fee2814e6629a8e86e872da00
vincentappiah <appiahv@rocketmail.com>
2021-03-02 20:24:17 +0000

    Update draw_phylo.py

--- draw_phylo.py
+++ draw_phylo.py
@@ -2,6 +2,7 @@
 
 from Bio import Phylo
 import sys
+import os
 import matplotlib
 matplotlib.use('Agg')
 import pylab
@@ -13,8 +14,12 @@ file_=sys.argv[1]
 tree=Phylo.read(file_,'newick')
 Phylo.draw(tree,do_show=False)
 #plt.savefig('Phylo.jpg')
+
+workdir=os.getcwd()
+
 pylab.savefig('Phylo.jpg',dpi=1500)
 pylab.savefig('Phylo.svg',dpi=1500)
 
-print("Phylogenetic tree generated and saved Phylo.jpg")
 
+print("Phylogenetic tree generated and saved as Phylo.jpg and Phylo.svg")
+print("Phylogenetic tree generated and saved in directory:%s"%workdir)
